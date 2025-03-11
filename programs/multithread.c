33581bd9af766eec94c4d1d7593634d8a648e23a
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-03-11 02:02:25 -0400

    multithread.c

--- programs/multithread.c
+++ programs/multithread.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-#include <pthread.h>
+#include "syscall.h" 
 
-void* print_message(void* ptr) {
+void print_message(void* ptr) {
     char* message = (char*) ptr;
     printf("%s\n", message);
-    return NULL;
 }
 
 int main() {
-    pthread_t thread1, thread2;
+    int thread1, thread2;
     char *message1 = "Thread 1";
     char *message2 = "Thread 2";
 
-    pthread_create(&thread1, NULL, print_message, (void*) message1);
-    pthread_create(&thread2, NULL, print_message, (void*) message2);
+    // USES MentOS's built-in thread creation syscall
+    thread1 = create_thread(print_message, (void*)message1);
+    thread2 = create_thread(print_message, (void*)message2);
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    // Waiting for the threads to finish 
+    wait_thread(thread1);
+    wait_thread(thread2);
 
     return 0;
 }
