7ef999eb770319ced4b313c83f9f1053d85caa96
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2025-03-10 21:41:32 -0400

    shared_memory.c

---
+++ programs/shared_memory.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+
+int main() {
+    int shmid = shmget(IPC_PRIVATE, 1024, 0666|IPC_CREAT);
+    char *shm = (char*) shmat(shmid, NULL, 0);
+
+    if (fork() == 0) {
+        sprintf(shm, "Hello from child process!");
+        shmdt(shm);
+    } else {
+        wait(NULL);
+        printf("Parent process reads: %s\n", shm);
+        shmdt(shm);
+        shmctl(shmid, IPC_RMID, NULL);
+    }
+    return 0;
+}
